123b5ad531ca3ad7f58acfa89360b0c4c5372f79
sosoeeee <809953371@qq.com>
2024-02-22 15:27:50 +0800

    init ROS workspace and create pkg "env_publisher"

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/jun/pHRI_Shared_Control/src
+# Build directory: /home/jun/pHRI_Shared_Control/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jun/pHRI_Shared_Control/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/jun/miniconda3/envs/robot/bin:/home/jun/miniconda3/condabin:/home/jun/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/jun/.local/share/JetBrains/Toolbox/scripts'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig:/opt/ros/noetic/lib/x86_64-linux-gnu/pkgconfig:/usr/local/lib/pkgconfig'
+export PWD='/home/jun/pHRI_Shared_Control/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/jun/pHRI_Shared_Control/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/jun/pHRI_Shared_Control/src:$ROS_PACKAGE_PATH"
