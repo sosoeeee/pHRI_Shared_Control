753b55743d96878da2ea9da6fa0a4ca6ed026c34
sosoeeee <809953371@qq.com>
2024-03-16 20:04:00 +0800

    add a filter on human signal

--- src/controller/scripts/BaseController.py
+++ src/controller/scripts/BaseController.py
@@ -14,6 +14,11 @@ class BaseController:
         # subscribe human command
         rospy.Subscriber("stickSignal", String, self.humanCmd_callback, queue_size=1)
         self.humanCmd = np.zeros((6, 1))
+        self.rawBuffer = np.zeros((6, 3))
+        self.filterBuffer = np.zeros((6, 3))
+        # Cutoff Frequency is 10Hz
+        self.filter_b = [0.0675, 0.1349, 0.0675]
+        self.filter_a = [1, -1.1430, 0.4128]
 
         # subscribe robot states from actuator
         rospy.Subscriber('/actuator/robotState', StateVector, self.cartesianState_callBack, queue_size=1)
@@ -37,14 +42,33 @@ class BaseController:
 
     def humanCmd_callback(self, msg):
         posAndForce = msg.data.split(',')
-        # haptic stick pos (x, y, z)
-        self.humanCmd[0] = float(posAndForce[0])
-        self.humanCmd[1] = float(posAndForce[1])
-        self.humanCmd[2] = float(posAndForce[2])
-        # interactive force (x, y, z) between human and remote haptic device
-        self.humanCmd[3] = -float(posAndForce[3])
-        self.humanCmd[4] = float(posAndForce[5])
-        self.humanCmd[5] = -float(posAndForce[4])
+        # The sampling frequency of haptic device signal is 100Hz
+        # update raw buffer
+        self.rawBuffer[:, 2] = self.rawBuffer[:, 1]
+        self.rawBuffer[:, 1] = self.rawBuffer[:, 0]
+        self.rawBuffer[:, 0] = np.array([float(posAndForce[0]), float(posAndForce[1]), float(posAndForce[2]),
+                                         -float(posAndForce[3]), float(posAndForce[5]), -float(posAndForce[4])]).reshape(6, 1)
+        # update filter buffer
+        self.filterBuffer[:, 2] = self.filterBuffer[:, 1]
+        self.filterBuffer[:, 1] = self.filterBuffer[:, 0]
+        self.filterBuffer[:, 0] = (self.filter_b[0] * self.rawBuffer[:, 0] +
+                                   self.filter_b[1] * self.rawBuffer[:, 1] +
+                                   self.filter_b[2] * self.rawBuffer[:, 2] -
+                                   self.filter_a[1] * self.filterBuffer[:, 1] -
+                                   self.filter_a[2] * self.filterBuffer[:, 2]) / self.filter_a[0]
+
+        # filter data
+        self.humanCmd = np.array([data if abs(data) > 0.001 else 0 for data in self.filterBuffer[:, 0]]).reshape(6, 1)
+
+        # posAndForce = msg.data.split(',')
+        # # haptic stick pos (x, y, z)
+        # self.humanCmd[0] = float(posAndForce[0])
+        # self.humanCmd[1] = float(posAndForce[1])
+        # self.humanCmd[2] = float(posAndForce[2])
+        # # interactive force (x, y, z) between human and remote haptic device
+        # self.humanCmd[3] = -float(posAndForce[3])
+        # self.humanCmd[4] = float(posAndForce[5])
+        # self.humanCmd[5] = -float(posAndForce[4])
 
     def cartesianState_callBack(self, msg):
         self.currentStates[0] = msg.x
--- src/task_publisher/scripts/BaseTaskServer.py
+++ src/task_publisher/scripts/BaseTaskServer.py
@@ -15,6 +15,11 @@ class BaseTaskServer(object):
         # subscribe human force
         rospy.Subscriber("stickSignal", String, self.humanCmd_callback, queue_size=1)
         self.humanForce = np.zeros((3, 1))
+        self.rawBuffer = np.zeros((3, 3))
+        self.filterBuffer = np.zeros((3, 3))
+        # Cutoff Frequency is 10Hz
+        self.filter_b = [0.0675, 0.1349, 0.0675]
+        self.filter_a = [1, -1.1430, 0.4128]
 
         # subscribe robot states from actuator
         rospy.Subscriber('/actuator/robotState', StateVector, self.cartesianState_callBack, queue_size=1)
@@ -22,10 +27,28 @@ class BaseTaskServer(object):
 
     def humanCmd_callback(self, msg):
         posAndForce = msg.data.split(',')
-        # interactive force (x, y, z) between human and remote haptic device
-        self.humanForce[0] = -float(posAndForce[3])     
-        self.humanForce[1] = float(posAndForce[5])
-        self.humanForce[2] = -float(posAndForce[4])
+        # The sampling frequency of haptic device signal is 100Hz
+        # update raw buffer
+        self.rawBuffer[:, 2] = self.rawBuffer[:, 1]
+        self.rawBuffer[:, 1] = self.rawBuffer[:, 0]
+        self.rawBuffer[:, 0] = np.array([-float(posAndForce[3]), float(posAndForce[5]), -float(posAndForce[4])]).reshape(3, 1)
+        # update filter buffer
+        self.filterBuffer[:, 2] = self.filterBuffer[:, 1]
+        self.filterBuffer[:, 1] = self.filterBuffer[:, 0]
+        self.filterBuffer[:, 0] = (self.filter_b[0] * self.rawBuffer[:, 0] +
+                                   self.filter_b[1] * self.rawBuffer[:, 1] +
+                                   self.filter_b[2] * self.rawBuffer[:, 2] -
+                                   self.filter_a[1] * self.filterBuffer[:, 1] -
+                                   self.filter_a[2] * self.filterBuffer[:, 2]) / self.filter_a[0]
+
+        # filter data
+        self.humanForce = np.array([data if abs(data) > 0.001 else 0 for data in self.filterBuffer[:, 0]]).reshape(3, 1)
+
+        # posAndForce = msg.data.split(',')
+        # # interactive force (x, y, z) between human and remote haptic device
+        # self.humanForce[0] = -float(posAndForce[3])
+        # self.humanForce[1] = float(posAndForce[5])
+        # self.humanForce[2] = -float(posAndForce[4])
 
     def cartesianState_callBack(self, msg):
         self.currentStates[0] = msg.x
