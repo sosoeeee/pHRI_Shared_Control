b71b70a04574b03eb39f02c28be102da1309189b
sosoeeee <809953371@qq.com>
2024-04-20 13:09:57 +0800

    Revert "integrate teb local planner into system"
    This reverts commit 05318a40ce2da7c0d0ed1088e37463f5e33eea19.

--- src/local_planner/include/teb_local_planner.h
+++ src/local_planner/include/teb_local_planner.h
@@ -28,8 +28,6 @@ private:
     // spacial scale
     float spacial_scale;
     boost::shared_ptr< dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig> > dynamic_recfg;
-    int plan_times;
-    std::vector<geometry_msgs::PoseStamped> init_plan;
 public:
     TebLocalPlanner();
     ~TebLocalPlanner();
